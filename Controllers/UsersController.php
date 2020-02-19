e05b947ced4123c5a1486ebe1c2bd755a1fcacf0
oumar <oumarkante301@gmail.com>
2020-02-19 17:25:46 +0100

    gg

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -1,27 +1,19 @@
+
+<script src="https://code.jquery.com/jquery-latest.js" ></script>
 <?php
 require_once './Models/User.php ';
-require_once 'Api_Ebrigade/Controllers/UtilisateurController.php';
 
 class UsersController{
 
 static public function getAllUsers()
 {
-    /*
-    ob_start();
-    $users = UtilisateurController::ListUtilisateur();
-    include ("Views/home.php");
-    $html = ob_end_flush();
-    return $html;
-    */
-
-$users = UtilisateurController::ListUtilisateur();
-return $users;
-
+    $users = file_get_contents("http://localhost/api/utilisateurs.php?c=utilisateurs&m=SearchUtilisateur&P_CODE=55t5");
+    return json_decode($users,true);
 }
 
 
 public function addUser(){
- 
+
 if(isset($_POST['submit'])){
 
 $data= array(
@@ -60,4 +52,23 @@ return $user;
 }
 
 }
+
+
+/*
+   ob_start();
+   $users = UtilisateurController::ListUtilisateur();
+   include ("Views/home.php");
+   $html = ob_end_flush();
+   return $html;
+     ?><script>
+                   $.get("http://localhost/api/utilisateurs.php")
+                       .done((data) => console.log(data))
+                       .fail((error) => console.error(error))
+                   always(() => console.log('done'));
+
+               </script><?php
+
+   */
 ?>
+
+
--- DATABASE/DB.php
+++ DATABASE/DB.php
@@ -1,26 +1,10 @@
 <?php
-
 class DB {
-static public function connect(){
-  
-
-        $db = new PDO("mysql:host=localhost;dbname=ebrigade","root","");
-        $db->exec("set names utf8");
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-        
-        
-
-    return $db;
-    
-
+    static public function connect(){
+            $db = new PDO("mysql:host=localhost;dbname=ebrigade","root","");
+            $db->exec("set names utf8");
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        return $db;
+    }
 }
-
-   
-
-}
-
-
-
-
-
 ?>
