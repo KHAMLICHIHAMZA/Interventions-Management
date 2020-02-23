89706101b016832c18d75af67a94aa555e9633a3
Hmizo <hamza.khamlichi@uha.fr>
2020-02-23 14:33:50 +0100

    on progress

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -7,10 +7,6 @@ class UsersController{
 
 public static function getAllUsers()
 {
-<<<<<<< HEAD
-    $users = file_get_contents("http://localhost/API-Ebrigade-Interventions/utilisateurs.php?c=utilisateurs&m=ListUtilisateur");
-    return json_decode($users,true);
-=======
     
     $users = file_get_contents("http://localhost/api/utilisateurs.php?c=utilisateurs&m=ListUtilisateur");
    
@@ -24,7 +20,6 @@ public static function getOneUser()
  $users = file_get_contents("http://localhost/api/utilisateurs.php?c=utilisateurs&m=getOne");
  return json_decode($users,true);
 
->>>>>>> b4e66e36dfa28b9d26f029a7bf5faf733e8d6872
 }
 public static function update(){
 
